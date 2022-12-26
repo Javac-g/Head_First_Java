09b27cf186108ba469f15eaaded00fe5e46f60ed
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-12-26 02:22:48 +0200

    data

--- src/com/company/Ch1/Reader.java
+++ src/com/company/Ch1/Reader.java
@@ -2,5 +2,13 @@ package com.company.Ch1;
 
 public class Reader {
 
-
+    public static void main(String...args){
+        int x = 5;
+        while (x > 1){
+            x--;
+            if (x < 3){
+                System.out.println("Smaller X");
+            }
+        }
+    }
 }
