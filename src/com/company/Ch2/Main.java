8d7ce24a1126207775a2e01a2531b248aff98202
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-12-26 16:29:59 +0200

    Chapter 2

---
+++ src/com/company/Ch2/Main.java
@@ -0,0 +1,26 @@
+package com.company.Ch2;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+    public static Shape run(){
+        System.out.println("Enter number of Angles");
+        int D = scanner.nextInt();
+        Shape ob;
+
+        switch (D){
+            case 3-> ob = new Triangle();
+            case 4 ->ob = new Square();
+            case 0 ->ob = new Circle();
+            default -> ob = new Amoeba();
+        }
+        return ob;
+    }
+    public static void main(String...args){
+        Shape ob = run();
+        ob.singing();
+        ob.rotate();
+    }
+}
