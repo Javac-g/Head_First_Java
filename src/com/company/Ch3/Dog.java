93dc6efe3ea1cdf432c0b6437a69257936b31c80
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-12-27 18:54:07 +0200

    Ch3 dogs

---
+++ src/com/company/Ch3/Dog.java
@@ -0,0 +1,44 @@
+package com.company.Ch3;
+
+public class Dog {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public  void bark(){
+        System.out.println("Bark bark");
+    }
+    public static void main(String...args){
+        Dog dog1 = new Dog();
+
+        dog1.setName("Bart");
+        dog1.bark();
+
+        Dog[] myDogs = new Dog[5];
+
+        myDogs[0] = new Dog();
+        myDogs[1] = new Dog();
+        myDogs[2] = new Dog();
+        myDogs[3] = dog1;
+
+        myDogs[0].setName("Fred");
+        myDogs[1].setName("Marge");
+        myDogs[2].setName("Den");
+        myDogs[3].setName("Jhonn");
+
+        System.out.println("Last dog name is: " + myDogs[3].getName());
+        int x = 0;
+        while (x < myDogs.length-1){
+            myDogs[x].bark();
+            x+=1;
+        }
+
+
+    }
+}
