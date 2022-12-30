9d29e38fdcd52f658fa7a1a0caff906f465fbe08
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-12-30 23:51:06 +0200

    WarShips done

--- src/com/company/Ch5/GameLauncher.java
+++ src/com/company/Ch5/GameLauncher.java
@@ -1,5 +1,6 @@
 package com.company.Ch5;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class GameLauncher {
@@ -13,7 +14,7 @@ public class GameLauncher {
 
         for (int i = 0; i < gameSetup.getBattleField().size();i++){
             if (!gameSetup.getBattleField().get(i).getAlive()){
-                life--;
+                --life;
             }
         }
     }
@@ -28,6 +29,13 @@ public class GameLauncher {
             if (x.getLife() == 0){
                 x.setAlive(false);
                 System.out.println("Killed: " + x.getName());
+
+            }
+            if (!x.getCoords().get(i).equals(damagedArea)){
+                if(!x.getAlive()){
+                    continue;
+                }
+                System.out.println(x.getName() + ": MISS");
             }
             for (int j = 0;j < x.getCoords().size();j++){
                 if (x.getCoords().get(j).equals(damagedArea)){
@@ -40,6 +48,7 @@ public class GameLauncher {
 
     public static void main(String...args){
         life = gameSetup.getBattleField().size();
+        System.out.println("Life: " + life);
 
 
         System.out.println("\nSea buttle");
--- src/com/company/Ch5/GameSetup.java
+++ src/com/company/Ch5/GameSetup.java
@@ -2,14 +2,22 @@ package com.company.Ch5;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class GameSetup {
     private final List<Ship> battleField = new ArrayList<>();
-    private final String[] coords = {"A","B","C","D","E","F","G"};
+    private List<String> coords =new ArrayList();
 
     public GameSetup() {
+        coords.add("A");
+        coords.add("B");
+        coords.add("C");
+        coords.add("E");
+        coords.add("F");
+        coords.add("G");
+
         Ship s1 = new Ship("Mariner");
         Ship s2 = new Ship("Interceptor");
         Ship s3 = new Ship("Flagman");
@@ -31,30 +39,35 @@ public class GameSetup {
         return battleField;
     }
 
+
     public void setShipsCoords(Ship s){
+
         Random r = new Random();
         String cx;
 
         int x,y,z;
 
-        x = r.nextInt(0,7);
-        cx = coords[x] + x;
+        x = r.nextInt(coords.size());
+
+        cx = coords.get(x) + x;
+
+        System.out.println(coords);
         String cy = "";
         String cz = "";
 
 
         if(x >= 5){
             y = x-1;
-            cy =coords[x] + y ;
+            cy = coords.get(x) + y ;
             z = y-1;
-            cz = coords[x] + z ;
+            cz = coords.get(x) + z ;
         } else {
             y = x + 1;
             z = y + 1;
-            cy = coords[x] + y ;
-            cz = coords[x] + z ;
+            cy = coords.get(x) + y ;
+            cz = coords.get(x) + z ;
         }
-
+        coords.remove(x);
 
 
 
